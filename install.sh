51476807fd766f0c26ee7065f8148fb43d3dfa39
Aditya <adityajasud8@gmail.com>
2025-06-01 20:05:00 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 [ -z "$TERMUX_VERSION" ] && echo -e "Termux not detected !!" && exit 1
-BIN="$PREFIX/bin/revancify_Enhance"
-curl -sL "https://github.com/Graywizard888/Revancify_Enhance/raw/refs/heads/main/revancify_Enhance" -o "$BIN"
+BIN="$PREFIX/bin/enhancify"
+curl -sL "https://github.com/Graywizard888/Revancify_Enhance/raw/refs/heads/main/enhancify" -o "$BIN"
 [ -e "$BIN" ] && chmod +x "$BIN" && "$BIN"
