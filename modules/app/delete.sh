9d633d6ff6ea421d30a9209a33031f341bbca103
Aditya <adityajasud8@gmail.com>
2025-10-28 21:42:58 +0530

    Update delete.sh

--- modules/app/delete.sh
+++ modules/app/delete.sh
@@ -2,7 +2,7 @@
 
 deleteApps() {
     if "${DIALOG[@]}" \
-        --title '| Delete Assets |' \
+        --title '| Delete Apps |' \
         --defaultno \
         --yesno "Please confirm to delete the apps.\nIt will delete all the downloaded and patched apps." -1 -1; then
         rm -rf "apps"/* "$STORAGE"/Patched/* &> /dev/null
