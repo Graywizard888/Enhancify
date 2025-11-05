4ddfc6677aa4bcbab7277e00ac1ab42562d85c29
Aditya <adityajasud8@gmail.com>
2025-11-05 22:46:26 +0530

    Update assets.sh

--- modules/assets.sh
+++ modules/assets.sh
@@ -94,8 +94,8 @@ fetchAssetsInfo() {
         --compressed \
         --retry 3 \
         --retry-delay 1 \
-        -A "$USER_AGENT_GITHUB" \
         --resolve "api.github.com:443:${IP}" \
+        -A "User-Agent: $USER_AGENT_GITHUB" \
         -H "Accept: application/vnd.github+json" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
         -D headers.tmp)
